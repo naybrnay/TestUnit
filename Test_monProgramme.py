4bdfc402b15be2b78abe7d0cd9fc074bb1be226b
naybrnay <100835188+naybrnay@users.noreply.github.com>
2022-03-17 14:17:09 +0100

    Update Test_monProgramme.py

--- Test_monProgramme.py
+++ Test_monProgramme.py
@@ -5,7 +5,7 @@ class TestMonProgramme(unittest.TestCase):
 
     def testMultiplication(self):
         r = mp.multiplication(2,3)
-        self.assertEqual(r, 4)
+        self.assertEqual(r, 6)
 
 if __name__ == "__main__":
     unittest.main()
